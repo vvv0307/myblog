b069f3db88d5ea0bffd08bdf3b42ff1a54692704
vvvzht <vvvzht@localhost.localdomain>
2018-03-21 21:16:29 +0800

    update

---
+++ src/main/java/com/vvv/controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.vvv.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class HelloWorldController {
+    private static final Log logger = LogFactory.getLog(HelloWorldController.class);
+    @RequestMapping(value="/hello",method = RequestMethod.GET)
+    @ResponseBody
+    public String hello(HttpServletRequest request){
+        String s = request.getParameter("name");
+        return s;
+    }
+}
